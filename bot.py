082dd88fcea575ccc06e70725a074c730f60710e
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2024-04-18 01:03:11 +0400

    added ru => hy

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import os
-import time
 
-import requests
 import telebot
-from transliterate import translit, get_available_language_codes
+from transliterate import translit
+import re
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 bot = telebot.TeleBot(BOT_TOKEN)
 bot.timeout = 100  # Increase timeout as needed (in seconds)
+russian_pattern = re.compile(r'[А-Яа-яЁё]+')
 
 
 @bot.message_handler(func=lambda message: True)
@@ -17,17 +17,28 @@ def handle_message(message):
         # Extract the text from the message
         message_text = message.text
 
-        # Transliterate the message text to Armenian (hy)
-        transliterated_text = translit(message_text, 'hy')
+        # Check if the message contains Russian words
+        if russian_pattern.search(message_text):
+            # Translate the message text from Russian to Armenian
+            translated_text = translit(message_text, 'ru', reversed=True)
+            transliterated_text = translit(translated_text, 'hy')
 
-        # Reply to the user with the transliterated text
-        bot.reply_to(message, transliterated_text)
+            # Reply to the user with the translated text
+            bot.reply_to(message, transliterated_text)
+        else:
+            # Transliterate the message text to Armenian
+            transliterated_text = translit(message_text, 'hy')
 
-    except requests.exceptions.ReadTimeout:
-        # Log the error and retry after a short wait
-        print("ReadTimeout occurred, retrying...")
-        time.sleep(5)  # Wait for 5 seconds before retrying
-        handle_message(message)
+            # Reply to the user with the transliterated text
+            bot.reply_to(message, transliterated_text)
+    except AttributeError as e:
+        # Log the error and reply with an error message
+        print("Error translating message:", e)
+        bot.reply_to(message, "An error occurred during translation. Please try again later.")
+    except Exception as e:
+        # Handle other exceptions
+        print("An unexpected error occurred:", e)
+        bot.reply_to(message, "An unexpected error occurred. Please try again later.")
 
 
 bot.infinity_polling()
